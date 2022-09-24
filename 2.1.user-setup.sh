9270d25d0e51e404b34e60a94507dfa296c0024d
NazarK0 <staticman999@gmail.com>
2022-09-25 00:04:17 +0300

    split setup scripts

---
+++ 2.1.user-setup.sh
@@ -0,0 +1,23 @@
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+p10k configure
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+$usrhome/.zshrc >> plugins=(zsh-autosuggestions git)
+source ~/.zshrc
+
+git clone https://github.com/vinceliuice/Layan-cursors
+cd Layan-cursors
+sudo ./install.sh
+sudo mkdir -p /usr/share/icons/default/
+echo "[Icon Theme]" | sudo tee /usr/share/icons/default/index.theme > /dev/null
+echo "Inherits=Layan-cursors" | sudo tee -a /usr/share/icons/default/index.theme > /dev/null
+# Powermenu Unicode problem fix
+cd ../
+git clone https://github.com/adi1090x/rofi.git
+mkdir -p $usrhome/.local/share/fonts
+cp -rf rofi/fonts/* $usrhome/.local/share/fonts/
+
+
+rm -rf $builddir
+
+echo "Done!"
--- 2.user-setup.sh
+++ 2.user-setup.sh
@@ -15,26 +15,3 @@ cp dotfiles/sddm.conf /etc/sddm.conf
 #configure zsh
 chsh -s /bin/zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-p10k configure
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-$usrhome/.zshrc >> plugins=(zsh-autosuggestions git)
-source ~/.zshrc
-
-git clone https://github.com/vinceliuice/Layan-cursors
-cd Layan-cursors
-sudo ./install.sh
-sudo mkdir -p /usr/share/icons/default/
-echo "[Icon Theme]" | sudo tee /usr/share/icons/default/index.theme > /dev/null
-echo "Inherits=Layan-cursors" | sudo tee -a /usr/share/icons/default/index.theme > /dev/null
-# Powermenu Unicode problem fix
-cd ../
-git clone https://github.com/adi1090x/rofi.git
-mkdir -p $usrhome/.local/share/fonts
-cp -rf rofi/fonts/* $usrhome/.local/share/fonts/
-
-
-rm -rf $builddir
-
-echo "Done!"
