6a64d82ae39ee61b118ea8db25db8fab02fb5adc
NazarK0 <staticman999@gmail.com>
2022-09-25 14:44:35 +0300

    bug fix setup script

--- 1.root-setup.sh
+++ 1.root-setup.sh
@@ -8,16 +8,13 @@ builddir=/tmp/build
 # Add to Debian backports branch
 cat ./sources.list >> /etc/apt/sources.list
 
-sleep 2
-
 sed -i 's/^deb cdrom:*/#/' /etc/apt/sources.list
-sleep 2
 
 # Update packages list
 apt update
 
-apt install sudo zip unzip picom bspwm deepin-terminal rofi sxhkd lxpolkit lxappearance pulseaudio -y
-apt install feh lxappearance libqt5svg5 qml-module-qtquick-controls qml-module-qtquick-controls2 x11-xserver-utils -y
+apt install sudo zip unzip picom bspwm deepin-terminal rofi sxhkd lxpolkit lxappearance pulseaudio xorg -y
+apt install feh lxappearance libqt5svg5 qml-module-qtquick-controls qml-module-qtquick-controls2 -y
 sudo apt -t bullseye-backports install polybar -y
 
 systemctl enable sddm
@@ -28,26 +25,24 @@ usermod -aG sudo $username
 mkdir -p /usr/share/sddm/themes
 mkdir -p /usr/share/icons/default/
 
-cp .Xresources /home/$username/
-cp .Xdefaults /home/$username/
+
 
 
 cp dotfiles/sddm.conf /etc/sddm.conf
 tar -xzvf sugar-candy.tar.gz -C /usr/share/sddm/themes
-sleep 2
+sleep 5
 
 cd $builddir
-sleep 2
 
 # Download Nordic Theme
 git clone https://github.com/EliverLara/Nordic.git --depth=1 /usr/share/themes/
+sleep 5
 
 # Fira Code Nerd Font variant needed
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/FiraCode.zip
 unzip FiraCode.zip -d /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/Meslo.zip
 unzip Meslo.zip -d /usr/share/fonts
-cp /usr/share/fonts/* /usr/share/fonts
 git clone https://github.com/powerline/fonts.git --depth=1
 ./fonts/install.sh
 fc-cache -vf
--- 2.0.user-setup.sh
+++ 2.0.user-setup.sh
@@ -13,27 +13,35 @@ sudo chown -R $USER:$USER $HOME
 sudo apt install zsh papirus-icon-theme fonts-noto-color-emoji fonts-firacode fonts-material-design-icons-iconfont -y
 sudo apt install firefox-esr neofetch -y
 
+cp .Xresources $HOME/
+cp .Xdefaults $HOME/
 cp -R dotfiles/* $HOME/.config/
 cp background.jpg $HOME/Pictures/
 
 git clone https://github.com/vinceliuice/Layan-cursors.git /tmp/cursors/
 # cd Layan-cursors
 sudo /tmp/cursors/install.sh
+sleep 5
 
 echo "[Icon Theme]" | sudo tee /usr/share/icons/default/index.theme > /dev/null
 echo "Inherits=Layan-cursors" | sudo tee -a /usr/share/icons/default/index.theme > /dev/null
+sleep 5
 # Powermenu Unicode problem fix
 cd ../
 git clone https://github.com/adi1090x/rofi.git /tmp/rofi/
 mkdir -p $HOME/.local/share/fonts
 cp -rf /tmp/rofi/fonts/* $HOME/.local/share/fonts/
 
+xrdb $HOME/.Xresources
+xrdb -merge $HOME/.Xresources
+
+sleep 5
+
 #configure zsh
 chsh -s /bin/zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" -y
 
-xrdb $HOME/.Xresources
-xrdb -merge $HOME/.Xresources
+
 
 sudo reboot
 
