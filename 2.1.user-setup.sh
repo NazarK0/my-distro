5b11c83714ed0fc6582b3c24c8689749d68cdce3
NazarK0 <staticman999@gmail.com>
2022-09-25 10:34:38 +0300

    bug fix setup script

--- 2.1.user-setup.sh
+++ 2.1.user-setup.sh
@@ -1,8 +1,10 @@
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 sudo git clone https://github.com/romkatv/powerlevel10k.git --depth=1 $ZSH_CUSTOM/themes/powerlevel10k/ 
-sed -i 's#^ZSH_THEME="*"$#ZSH_THEME="powerlevel10k/powerlevel10k"#g' $HOME/.zshrc
+pattern = $(grep ^ZSH_THEME="*" $HOME/.zshrc)
+sed -i "s#$pattern#ZSH_THEME=\"powerlevel10k/powerlevel10k\"#g" $HOME/.zshrc
 
-source $HOME/.zshrc
+cd $HOME
+source .zshrc
 p10k configure
 # git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
