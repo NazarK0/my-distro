a98773456f205d3be60875b9bb881eef30247752
NazarK0 <staticman999@gmail.com>
2022-09-30 08:11:05 +0300

    add icons

--- 2.my-configs.sh
+++ 2.my-configs.sh
@@ -2,6 +2,7 @@
 
 mkdir -p $HOME/.config
 mkdir -p $HOME/.background
+mkdir -p $HOME/.icons
 mv ./config/.Xresources $HOME/.Xresources
 mv ./config/.Xdefaults $HOME/.Xdefaults
 mv ./config/background.jpg $HOME/.background/background.jpg
