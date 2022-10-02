96bcf65c22445a562a83e35cdbdfd6a8571a6575
NazarK0 <staticman999@gmail.com>
2022-10-02 12:19:47 +0300

    logging

--- 1.core-setup.sh
+++ 1.core-setup.sh
@@ -13,53 +13,53 @@ cat ./sources.list >> /etc/apt/sources.list
 sed -i 's/^deb cdrom:*/#/' /etc/apt/sources.list
 
 # Update packages list
-apt update 2>&1 | tee -a distro-install.log
+apt update >> distro-install.log 2>&1
 
-apt install sudo sddm bspwm pulseaudio feh psmisc picom sxhkd -y 2>&1 | tee -a distro-install.log
-apt install tint2 zip unzip x11-xserver-utils -y 2>&1 | tee -a distro-install.log
+apt install sudo sddm bspwm pulseaudio feh psmisc picom sxhkd -y >> distro-install.log 2>&1
+apt install tint2 zip unzip x11-xserver-utils -y >> distro-install.log 2>&1
 #dependencies for sddm theme 
-apt install libqt5svg5 qml‑module‑qtquick‑layouts qml-module-qtquick-controls qml-module-qtquick-controls2 qml‑module‑qtgraphicaleffects -y 2>&1 | tee -a distro-install.log
+apt install libqt5svg5 qml‑module‑qtquick‑layouts qml-module-qtquick-controls qml-module-qtquick-controls2 qml‑module‑qtgraphicaleffects -y >> distro-install.log 2>&1
 
 systemctl enable sddm
 systemctl set-default graphical.target
 
 # xmenu build dependencies
-apt install -y libimlib2-dev libx11-dev libxinerama-dev libxft-dev 2>&1 | tee -a distro-install.log
+apt install -y libimlib2-dev libx11-dev libxinerama-dev libxft-dev >> distro-install.log 2>&1
 # install xmenu
 git clone https://github.com/phillbush/xmenu.git --depth=1 $downloaddir/xmenu
 cd $downloaddir/xmenu
-make 2>&1 | tee -a distro-install.log
-make install 2>&1 | tee -a distro-install.log
+make >> distro-install.log 2>&1
+make install >> distro-install.log 2>&1
 cd $repodir
 
 #ncpamixer build dependencies
-apt install build-essential gcc libssl-dev libncurses-dev libpulse-dev -y 2>&1 | tee -a distro-install.log
+apt install build-essential gcc libssl-dev libncurses-dev libpulse-dev -y >> distro-install.log 2>&1
 #install cmake
 wget https://github.com/Kitware/CMake/releases/download/v3.21.0/cmake-3.21.0.tar.gz $downloaddir/cmake
 tar xvf $downloaddir/cmake/cmake-3.21.0.tar.gz
 cd $downloaddir/cmake/cmake-3.21.0 
-./bootstrap 2>&1 | tee -a distro-install.log
-gmake 2>&1 | tee -a distro-install.log
-make install 2>&1 | tee -a distro-install.log
+./bootstrap >> distro-install.log 2>&1
+gmake >> distro-install.log 2>&1
+make install >> distro-install.log 2>&1
 cd $repodir
 
 # install ncpamixer
 git clone https://github.com/fulhax/ncpamixer.git --depth=1 $downloaddir/ncpamixer
 cd $downloaddir/ncpamixer
-make USE_WIDE=True 2>&1 | tee -a distro-install.log
+make USE_WIDE=True >> distro-install.log 2>&1
 cd $repodir
 
 # install fonts
-apt install -y fonts-noto-color-emoji fonts-firacode fonts-font-awesome fonts-powerline 2>&1 | tee -a distro-install.log
+apt install -y fonts-noto-color-emoji fonts-firacode fonts-font-awesome fonts-powerline >> distro-install.log 2>&1
 cp ./fonts/* /usr/share/fonts
 
 wget https://github.com/ryanoasis/nerd-fonts/archive/refs/tags/v2.2.2.zip $downloaddir/nerd-fonts
 unzip $downloaddir/nerd-fonts/v2.2.2.zip -d /usr/share/fonts
 
-fc-cache -vf 2>&1 | tee -a distro-install.log
+fc-cache -vf >> distro-install.log 2>&1
 
 # install terminal emulator
-apt install -y kitty 2>&1 | tee -a distro-install.log
+apt install -y kitty >> distro-install.log 2>&1
 
 # user setup operations
 mkdir /home/$username/images
