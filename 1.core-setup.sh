3ee5a5243001251b3c1d42fafee2883a6630844a
NazarK0 <staticman999@gmail.com>
2022-10-02 12:50:54 +0300

    logging

--- 1.core-setup.sh
+++ 1.core-setup.sh
@@ -5,6 +5,7 @@ mkdir -p /distro-src/download
 username=$(id -u -n 1000)
 downloaddir=/distro-src/download
 repodir=/distro-src/my-distro
+logfile=/distro-src/distro-install.log
 
 echo 'Add to apt all necessary repos'
 cat ./sources.list >> /etc/apt/sources.list
@@ -13,54 +14,54 @@ echo 'Delete cdrom from apt source.list'
 sed -i 's/^deb cdrom:*/#/' /etc/apt/sources.list
 
 echo 'Update packages list...'
-apt update >> distro-install.log 2>&1
+apt update >> $logfile 2>&1
 
 echo 'Install core packages...'
-apt install sudo sddm bspwm pulseaudio feh psmisc picom sxhkd -y >> distro-install.log 2>&1
-apt install tint2 zip unzip x11-xserver-utils -y >> distro-install.log 2>&1
+apt install sudo sddm bspwm pulseaudio feh psmisc picom sxhkd -y >> $logfile 2>&1
+apt install tint2 zip unzip x11-xserver-utils -y >> $logfile 2>&1
 #dependencies for sddm theme 
-apt install libqt5svg5 qml‑module‑qtquick‑layouts qml-module-qtquick-controls qml-module-qtquick-controls2 qml‑module‑qtgraphicaleffects -y >> distro-install.log 2>&1
+apt install libqt5svg5 qml‑module‑qtquick‑layouts qml-module-qtquick-controls qml-module-qtquick-controls2 qml‑module‑qtgraphicaleffects -y >> $logfile 2>&1
 
-systemctl enable sddm
+systemctl enable sddm >> $logfile 2>&1
 systemctl set-default graphical.target
 
 echo 'install xmenu build dependencies...'
-apt install -y libimlib2-dev libx11-dev libxinerama-dev libxft-dev >> distro-install.log 2>&1
+apt install -y libimlib2-dev libx11-dev libxinerama-dev libxft-dev >> $logfile 2>&1
 echo 'install xmenu...'
-git clone https://github.com/phillbush/xmenu.git --depth=1 $downloaddir/xmenu >> distro-install.log 2>&1
+git clone https://github.com/phillbush/xmenu.git --depth=1 $downloaddir/xmenu >> $logfile 2>&1
 cd $downloaddir/xmenu
-make >> distro-install.log 2>&1
-make install >> distro-install.log 2>&1
+make >> $logfile 2>&1
+make install >> $logfile 2>&1
 cd $repodir
 
 echo 'install ncpamixer build dependencies...'
-apt install build-essential gcc libssl-dev libncurses-dev libpulse-dev -y >> distro-install.log 2>&1
+apt install build-essential gcc libssl-dev libncurses-dev libpulse-dev -y >> $logfile 2>&1
 echo 'install cmake...'
-wget https://github.com/Kitware/CMake/releases/download/v3.21.0/cmake-3.21.0.tar.gz $downloaddir/cmake >> distro-install.log 2>&1
-tar xvf $downloaddir/cmake/cmake-3.21.0.tar.gz  ‑C $downloaddir/cmake >> distro-install.log 2>&1
+wget https://github.com/Kitware/CMake/releases/download/v3.21.0/cmake-3.21.0.tar.gz $downloaddir/cmake >> $logfile 2>&1
+tar xvf $downloaddir/cmake/cmake-3.21.0.tar.gz  ‑C $downloaddir/cmake >> $logfile 2>&1
 cd $downloaddir/cmake/cmake-3.21.0 
-./bootstrap >> distro-install.log 2>&1
-gmake >> distro-install.log 2>&1
-make install >> distro-install.log 2>&1
+./bootstrap >> $logfile 2>&1
+gmake >> $logfile 2>&1
+make install >> $logfile 2>&1
 cd $repodir
 
 echo 'install ncpamixer...'
-git clone https://github.com/fulhax/ncpamixer.git --depth=1 $downloaddir/ncpamixer >> distro-install.log 2>&1
+git clone https://github.com/fulhax/ncpamixer.git --depth=1 $downloaddir/ncpamixer >> $logfile 2>&1
 cd $downloaddir/ncpamixer
-make USE_WIDE=True >> distro-install.log 2>&1
+make USE_WIDE=True >> $logfile 2>&1
 cd $repodir
 
 echo 'install fonts...'
-apt install -y fonts-noto-color-emoji fonts-firacode fonts-font-awesome fonts-powerline >> distro-install.log 2>&1
+apt install -y fonts-noto-color-emoji fonts-firacode fonts-font-awesome fonts-powerline >> $logfile 2>&1
 cp ./fonts/* /usr/share/fonts
 
-wget https://github.com/ryanoasis/nerd-fonts/archive/refs/tags/v2.2.2.zip $downloaddir/nerd-fonts  >> distro-install.log 2>&1
+wget https://github.com/ryanoasis/nerd-fonts/archive/refs/tags/v2.2.2.zip $downloaddir/nerd-fonts  >> $logfile 2>&1
 unzip $downloaddir/nerd-fonts/v2.2.2.zip -d /usr/share/fonts
 
-fc-cache -vf >> distro-install.log 2>&1
+fc-cache -vf >> $logfile 2>&1
 
 echo 'install terminal emulator...'
-apt install -y kitty >> distro-install.log 2>&1
+apt install -y kitty >> $logfile 2>&1
 
 # user setup operations
 echo 'update user home directory'
@@ -91,4 +92,4 @@ chown -R $username:users /home/$username/shared
 echo 'change grub default background'
 mv configuration/grub-bg.jpg /usr/share/grub/grub-bg.jpg
 echo 'GRUB_BACKGROUND=/usr/share/grub/grub-bg.jpg' >> /etc/default/grub
-update-grub >> distro-install.log 2>&1
+update-grub >> $logfile 2>&1
